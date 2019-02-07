2d330da0f8aeadba9cebd463f3e7c39f05e3a299
Marion LEHURAUX <marion.lehuraux@hotmail.fr>
2019-02-07 11:55:41 +0100

    Import

--- pyplot/src/hello.py
+++ pyplot/src/hello.py
@@ -9,5 +9,38 @@
 First program hello.py: display "Hello, world!" on standard output.
 """
 
+###
 print("Hello, world!")
 print("Best Binome Ever")
+# This is a test
+###
+
+import sys
+
+# By convention, a function name must contain only lowercase characters and _.
+# txt=None defines a default value for string and makes it optional.
+def PrintMsg(txt=None):
+    """Print a message received as an argument or a default message
+    if none is passed.
+
+    :param txt: a string to print (optional)
+    :return: status value (always success, 0)
+    """
+
+    if txt is None:
+        # Define a default message
+        txt = 'Hello, world!'
+    print("{}".format(txt))
+    return 0
+
+# The following test is considered as a best practice: this way a module
+# can be used both as a standalone application or as a module called by another
+# module.
+if __name__ == "__main__":
+
+    # The main program is implemented mainly as a function: this avoids having
+    # all the variables used in this context (e.g. text in print_msg) to
+    # become global variables.
+    status = PrintMsg()
+    status = PrintMsg("Coucou")
+    sys.exit(status)
