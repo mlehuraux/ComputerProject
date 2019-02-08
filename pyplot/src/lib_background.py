65d084ab58a1a48906304a3c07f40d3ef0ace865
Marion Lehuraux <marion.lehuraux@hotmail.fr>
2019-02-08 15:34:01 +0100

    final

--- pyplot/src/lib_background.py
+++ pyplot/src/lib_background.py
@@ -4,6 +4,7 @@ from npac import args
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as scp
 
 def create_histo(pixels, nbins):
     """Create histogram with nbins bins out of pixels 2D np.array"""
@@ -17,12 +18,6 @@ def create_histo(pixels, nbins):
     bin_values, bin_boundaries = np.histogram(histo, nbins)
     print("Histogram has been created !")
 
-    fig, main_axes = plt.subplots()
-    plt.hist(histo, nbins)
-    plt.xlabel("Amplitude", fontsize=16)
-    plt.ylabel("Frequency", fontsize=16)
-    plt.title("Flux distribution", fontsize=20)
-    plt.show()
     return 0
 
 def modelling_function(x, p1, p2, p3):
@@ -36,7 +31,61 @@ def modelling_function(x, p1, p2, p3):
     return y
 
 def apply_model(input_array, p1, p2, p3):
+    """
+    Compute expected vlue of modeling function on input_array
+    """
+
     output_array = []
-    for i in range(-10, 10):
+    for i in range(len(input_array)):
         output_array.append(modelling_function(input_array[i], p1, p2, p3))
     return(np.array(output_array))
+
+def normalize(bin_values, bin_boundaries):
+    """
+    Normalize the histogram
+    """
+    mval = np.float(np.max(bin_values))
+    mbound = np.float(np.max(bin_boundaries))
+    normal_val = bin_values/mval
+    normal_bound = bin_boundaries[:-1]/mbound
+    return(normal_val, normal_bound)
+
+def max_array(input):
+    """
+    Extract max vlue of input_array
+    """
+    return(np.float(np.max(input)))
+
+def fit_and_return(normal_bound, normal_val):
+    """
+    Fit the histogram and return un-normalized fit parameters
+    """
+    fit, covariant = scp.curve_fit(modelling_function, normal_bound, normal_val)
+
+    maxvalue = fit[0] * max_array(bin_values)
+    background = fit[1] * max_array(bin_boundaries)
+    dispersion = fit[2] * max_array(bin_boundaries)
+
+    return(maxvalue, background, dispersion)
+
+def plotting(x1, y1, y2):
+    """
+    Plot the original histogram and its fit
+    """
+    fig, main_axes = plt.subplots()
+    plt.plot(x1, y1, 'b+:', label='data')
+    plt.plot(x1, y2, 'r.:', label='fit')
+
+    plt.xlabel("Amplitude", fontsize=16)
+    plt.ylabel("Frequency", fontsize=16)
+    plt.title("Flux distribution", fontsize=20)
+
+    plt.legend()
+
+
+    plt.show()
+
+
+
+
+
