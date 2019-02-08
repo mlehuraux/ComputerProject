84c06eadd26efc0d3002a09d47ec93933c53debd
Marion LEHURAUX <marion.lehuraux@hotmail.fr>
2019-02-08 10:26:14 +0100

    Adapt file_name def

--- pyplot/src/ex1_read_image.py
+++ pyplot/src/ex1_read_image.py
@@ -12,55 +12,27 @@ def main():
     # Allowed names for file_name
     names = ["common", "specific", "global", "dss.16.54.38.6-76.08.22.5"]
 
-    # Check that file_name is correct. If not, write it again and specify interactive
-    try:
+    try: # check if file_name exists as given by user
         file_name, interactive = args.get_args()
-    except FileNotFoundError:
+    except FileNotFoundError: # user can correct if previously wrong
         print('Files does not exist, file_name should be in : {}'.format(names))
         file_name = input('Enter file_name : \n')
         file_name = '../data/' + file_name + '.fits'
-        interactive = input('Interactive mode [True/False] \n')
+        interactive = input('Interactive mode [True/False] \n') # adjustment needed to define interactive without calling get_args()
         if interactive == 'True':
             interactive = True
         else :
             interactive = False
 
-    # main tasks
+    header, pixels = lib_fits.read_first_image(file_name)
+    signature_fmt_1 = header.get("CRPIX1")
+    signature_fmt_2 = header.get("CRPIX2")
+    print('RESULT: CRPIX1 = {:.0f}'.format(signature_fmt_1))
+    print('RESULT: CRPIX2 = {:.0f}'.format(signature_fmt_2))
 
-    # Opennig the file, and check again it's name
-    try :
-        with fits.open(file_name) as fits_blocks:
-            block = fits_blocks[0]
-            pixels = block.data
-    except FileNotFoundError:
-        print("Error with file name")
-        exit()
-
-    #show figure
-    if interactive:
-        fig, main_axes = plt.subplots()
-        imgplot = main_axes.imshow(pixels)
-        plt.show()
-
-    # example of console output - please replace it with your solutions!
-    # ...
-    print('RESULT: file = {}'.format(file_name))
-    print('RESULT: interactive = {}'.format(interactive))
-
-    # graphic output
-    #if interactive:
-        # ...
-    #    pass
-
-    # end
     return 0
 
 
 if __name__ == '__main__':
     """ Execute exercise 1 """
-    #sys.exit(main())
-    header, pixels = lib_fits.read_first_image('common')
-    signature_fmt_1 = header.get("CRPIX1")
-    signature_fmt_2 = header.get("CRPIX2")
-    print('RESULT: CRPIX1 = {:.0f}'.format(signature_fmt_1))
-    print('RESULT: CRPIX2 = {:.0f}'.format(signature_fmt_2))
+    sys.exit(main())
