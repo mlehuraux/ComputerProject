5de0e051173368bd2dd61f6c33d276023c83770e
Marion LEHURAUX <marion.lehuraux@hotmail.fr>
2019-02-08 11:14:01 +0100

    Signature format

--- pyplot/src/ex1_read_image.py
+++ pyplot/src/ex1_read_image.py
@@ -29,18 +29,15 @@ def main():
     #show figure
     if interactive:
         fig, main_axes = plt.subplots()
-        imgplot = main_axes.imshow(pixels)
+        main_axes.imshow(pixels)
         plt.show()
 
-    # example of console output - please replace it with your solutions!
-    # ...
-    #print('RESULT: file = {}'.format(file_name))
-    #print('RESULT: interactive = {}'.format(interactive))
+    # Signature
+    signature_fmt_1 = 'RESULT: CRPIX1 = {:.0f}'.format(header.get("CRPIX1"))
+    signature_fmt_2 = 'RESULT: CRPIX2 = {:.0f}'.format(header.get("CRPIX2"))
+    print(signature_fmt_1)
+    print(signature_fmt_2)
 
-    signature_fmt_1 = header.get("CRPIX1")
-    signature_fmt_2 = header.get("CRPIX2")
-    print('RESULT: CRPIX1 = {:.0f}'.format(signature_fmt_1))
-    print('RESULT: CRPIX2 = {:.0f}'.format(signature_fmt_2))
 
     return 0
 
